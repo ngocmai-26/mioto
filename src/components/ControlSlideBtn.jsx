037b32d80185fe8ecb16b4b727551817313508f3
ngocmai-26 <80842972+ngocmai-26@users.noreply.github.com>
2023-03-10 00:37:11 +0700

    update wwebsite

---
+++ src/components/ControlSlideBtn.jsx
@@ -0,0 +1,9 @@
+export const ControlSlideBtn = (props) => {
+  return (
+    <div>
+      <button className="styles-module_carousel-arrow__26sRw">
+        {props.children}
+      </button>
+    </div>
+  );
+};
---
+++ src/components/NextSlideBtn.jsx
@@ -0,0 +1,9 @@
+import { ControlSlideBtn } from "./ControlSlideBtn";
+
+export const NextSlideBtn = (props) => {
+  return (
+      <span className="ion icon here">
+        <i className="fa-regular fa-angle-left"></i>
+      </span>
+  );
+};
